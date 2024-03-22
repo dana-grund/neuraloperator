1595d900da92e345e959bd5a562df00374fc9468
Florian Stuehlinger <fstuehlinger@eu-lo-s4-067.euler.ethz.ch>
2024-03-22 12:18:18 +0100

    Added imports for new functions and classes

--- neuralop/losses/__init__.py
+++ neuralop/losses/__init__.py
@@ -1,3 +1,4 @@
-from .data_losses import LpLoss, H1Loss
+from .data_losses import LpLoss, H1Loss, MedianAbsoluteLoss
 from .equation_losses import BurgersEqnLoss, ICLoss
 from .meta_losses import WeightedSumLoss
+from .probabilistic_scores import gaussian_crps, compute_probabilistic_scores, cross_entropy
