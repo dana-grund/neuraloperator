b4b5b7aca36878205bbc8cebd638a18f39f2fa74
Florian Stuehlinger <fstuehlinger@eu-lo-s4-067.euler.ethz.ch>
2024-03-22 12:19:19 +0100

    Adjustments for new ensemble dimensions for deterministic data and added computation and printing of new losses for evaluation

--- examples/plot_FNO_shear.py
+++ examples/plot_FNO_shear.py
@@ -13,25 +13,40 @@ import argparse
 import sys
 import os
 import time
+import zarr
 
 from neuralop.models import TFNO
 from neuralop import Trainer
-from neuralop.datasets import load_shear_flow, plot_shear_flow_test
+from neuralop.datasets import load_shear_flow, plot_shear_flow_test, compute_deterministic_scores, print_scores
 from neuralop.utils import count_model_params
-from neuralop import LpLoss, H1Loss
+from neuralop import LpLoss, H1Loss, MedianAbsoluteLoss, gaussian_crps, compute_probabilistic_scores, cross_entropy
 
 parser = argparse.ArgumentParser(description='Train FNO for 2D shear')
-parser.add_argument('--res', type=int, default=64, required=False,
-                    help='training resolution (64 or 128)')
+parser.add_argument('--ensemble', action=argparse.BooleanOptionalAction, default=False, required=False,
+                    help='Use ensebmble data. Default: False')
+parser.add_argument('--res', type=int, default=128, required=False,
+                    help='training resolution (64 or 128). Default: 128')
 parser.add_argument('--gpu', action=argparse.BooleanOptionalAction, default=False, required=False,
                     help='Device to run on. Default (False): cpu')
 parser.add_argument('-f', '--folder', type=str, default='plot_FNO_shear', required=False,
                     help='Where to store results.')
 args = parser.parse_args()
 
+if args.ensemble and args.res == 64:
+    raise Exception('Ensemble data only available in 128.')
+
 device = 'cuda' if args.gpu else 'cpu'
 folder = args.folder
 res = args.res
+ensemble = args.ensemble
+
+# Print gpu model
+if args.gpu:
+    print(torch.cuda.get_device_name())
+    
+
+zarr.consolidate_metadata("/cluster/work/climate/webesimo/data_N128.zarr")
+zarr.consolidate_metadata("/cluster/work/climate/webesimo/data_N64.zarr")
 
 # %%
 # Load the Navier--Stokes dataset
@@ -42,7 +57,8 @@ train_loader, test_loaders, data_processor = load_shear_flow(
         test_resolutions=[64],  # [64,128], 
         n_tests=[10],           # [10_000, 10_000],
         test_batch_sizes=[32],  # [32, 32],
-        positional_encoding=True
+        positional_encoding=True,
+        ensemble=ensemble
 )
 data_processor = data_processor.to(device)
 
@@ -75,11 +91,21 @@ scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30)
 
 # %%
 # Create the losses
-l2loss = LpLoss(d=2, p=2)
-h1loss = H1Loss(d=2)
+reduce_dims = 0
+reductions = 'mean'
+l2loss = LpLoss(d=2, p=2, reduce_dims=reduce_dims, reductions=reductions)
+h1loss = H1Loss(d=2, reduce_dims=reduce_dims, reductions=reductions)
+l1loss = LpLoss(d=2, p=1, reduce_dims=reduce_dims, reductions=reductions)
+medianloss = MedianAbsoluteLoss(d=2, reduce_dims=reduce_dims, reductions=reductions)
 
 train_loss = h1loss
-eval_losses = {'l2': l2loss} # {'h1': h1loss, 'l2': l2loss}
+eval_losses = {'l2': l2loss, 'h1': h1loss, 'l1': l1loss, 'median absolute': medianloss} # {'h1': h1loss, 'l2': l2loss}
+
+# Probabilistic score metrics
+crps = gaussian_crps(member_dim=0, reduce_dims=None)
+#crossEntropy = cross_entropy(member_dim=0)
+
+probab_scores = {'crps': crps}
 
 
 # %%
@@ -118,19 +144,38 @@ model.save_checkpoint(save_folder=folder, save_name='example_fno_shear')
 end = time.time()
 print(f'Training took {end-start} s.')
 
+# Test 
+test_db = test_loaders[64].dataset
+model = TFNO.from_checkpoint(save_folder=folder, save_name='example_fno_shear')
+model.to(device)
+absScores, relScores = compute_deterministic_scores(
+    test_db,
+    model,
+    data_processor,
+    eval_losses
+)
+
+probScores = compute_probabilistic_scores(
+    test_db,
+    model,
+    data_processor,
+    probab_scores
+)
+
+print_scores(absScores, relScores, probScores, reductions)
+
+
 
 # %%
 # Plot the prediction, and compare with the ground-truth 
 # Note that this is a minimal working example for debugging only
 # In practice we would train a Neural Operator on one or multiple GPUs
-test_db = test_loaders[64].dataset
-model = TFNO.from_checkpoint(save_folder=folder, save_name='example_fno_shear')
 plot_shear_flow_test(
     test_db,
     model,
     data_processor,
     n_plot=5,
     save_file=os.path.join(
-        folder,'fig-example_shear.png'
+        folder,'fig-example_shear_n_train=10_n_epochs=2_gpu.png'
     ),
 )
