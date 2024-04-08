7cdf82253dddbcf4a86f8fb750a09b13496ce5d6
fstuehlinger <fstuehlinger@student.ethz.ch>
2024-04-08 16:02:30 +0200

    Adjustment for new function

--- neuralop/__init__.py
+++ neuralop/__init__.py
@@ -5,4 +5,4 @@ from .models import get_model
 from . import datasets
 from . import mpu
 from .training import Trainer, CheckpointCallback
-from .losses import LpLoss, H1Loss, BurgersEqnLoss, ICLoss, WeightedSumLoss, MedianAbsoluteLoss, gaussian_crps, compute_probabilistic_scores, cross_entropy
+from .losses import LpLoss, H1Loss, BurgersEqnLoss, ICLoss, WeightedSumLoss, MedianAbsoluteLoss, gaussian_crps, compute_probabilistic_scores, evaluate_ensemble
