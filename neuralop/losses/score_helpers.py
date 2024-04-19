6201243cffb756eeb08334bc04a3316b7b3311f8
fstuehlinger <fstuehlinger@student.ethz.ch>
2024-04-19 18:32:16 +0200

    Added plotting function for training errors

--- neuralop/losses/score_helpers.py
+++ neuralop/losses/score_helpers.py
@@ -1,5 +1,7 @@
 import torch
 from tabulate import tabulate
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def compute_deterministic_scores(
@@ -78,3 +80,96 @@ def print_scores(
         prob_table = [prob_row1, prob_row2]
         print(tabulate(prob_table, headers='firstrow', tablefmt='fancy_grid'))
     
+def plot_scores(
+    scores_det=None,
+    scores_prob=None
+):
+    """
+    """
+    if scores_det is None and scores_prob is not None:
+        fig, axs = plt.subplots(1)
+        fig.tight_layout(pad=3.0)
+        fig.set_size_inches(8,7)
+        probMat = dictToArray(scores_prob=scores_prob)
+        for scoreIdx in range(probMat.shape[0]):
+            x = np.arange(probMat.shape[1])
+            axs.plot(x, probMat[scoreIdx,:], label=list(scores_prob[0].keys())[scoreIdx])
+        axs.set(xlabel='Epoch', ylabel='Error')
+        axs.set_xticks(range(probMat.shape[1]))
+        axs.legend()
+        axs.set_title('Probabilistic errors')
+        
+    elif scores_det is not None and scores_prob is None:
+        fig, axs = plt.subplots(1)
+        fig.tight_layout(pad=3.0)
+        fig.set_size_inches(8,7)
+        detMat = dictToArray(scores_det=scores_det)
+        for scoreIdx in range(detMat.shape[0]):
+            x = np.arange(detMat.shape[1])
+            axs.plot(x, detMat[scoreIdx,:], label=list(scores_det[0].keys())[scoreIdx])
+        axs.set(xlabel='Epoch', ylabel='Error')
+        axs.set_xticks(range(detMat.shape[1]))
+        axs.legend()
+        axs.set_title('Deterministic errors')
+        
+    elif scores_det is not None and scores_prob is not None:
+        fig, axs = plt.subplots(2)
+        fig.tight_layout(pad=3.0)
+        fig.set_size_inches(8,7)
+        detMat, probMat = dictToArray(scores_det=scores_det, scores_prob=scores_prob)
+        for scoreIdx in range(detMat.shape[0]):
+            x = np.arange(detMat.shape[1])
+            axs[0].plot(x, detMat[scoreIdx,:], label=list(scores_det[0].keys())[scoreIdx])
+        for scoreIdx in range(probMat.shape[0]):
+            x = np.arange(probMat.shape[1])
+            axs[1].plot(x, probMat[scoreIdx,:], label=list(scores_prob[0].keys())[scoreIdx])
+        for ax in axs.flat:
+            ax.set(xlabel='Epoch', ylabel='Error')
+            ax.set_xticks(range(detMat.shape[1]))
+            ax.legend()
+        axs[0].set_title('Deterministic errors')
+        axs[1].set_title('Probabilistic errors')
+    plt.savefig("train_errors.png")
+    
+    
+def dictToArray(
+    scores_det=None,
+    scores_prob=None
+):
+    """
+    """
+    if scores_det is not None and scores_prob is not None:
+        detMat = np.zeros((len(scores_det[0].values()), len(scores_det)))
+        probMat = np.zeros((len(scores_prob[0].values()), len(scores_prob)))
+        for epoch in range(len(scores_det)):
+            scoreIdxDet = 0
+            scoreIdxProb = 0
+            for detScore in scores_det[epoch].values():
+                detMat[scoreIdxDet, epoch] = detScore
+                scoreIdxDet += 1
+                print(detScore)
+            for probScore in scores_prob[epoch].values():
+                probMat[scoreIdxProb, epoch] = probScore
+                scoreIdxProb += 1
+        
+        return detMat, probMat
+        
+    elif scores_det is None and scores_prob is not None:
+        probMat = np.zeros((len(scores_prob[0].values()), len(scores_prob)))
+        for epoch in range(len(scores_prob)):
+            scoreIdx = 0
+            for probScore in scores_prob[epoch].values():
+                probMat[scoreIdx, epoch] = probScore
+                scoreIdx += 1
+                
+        return probMat
+                
+    elif scores_det is not None and scores_prob is None:
+        detMat = np.zeros((len(scores_det[0].values()), len(scores_det)))
+        for epoch in range(len(scores_det)):
+            scoreIdx = 0
+            for detScore in scores_det[epoch].values():
+                detMat[scoreIdx, epoch] = detScore
+                scoreIdx += 1
+                
+        return detMat
