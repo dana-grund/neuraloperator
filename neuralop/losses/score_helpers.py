a138b1e3d203eaa7972761ba0024b0e0e284903e
fstuehlinger <fstuehlinger@student.ethz.ch>
2024-09-10 18:56:44 +0200

    Final version with comments

--- neuralop/losses/score_helpers.py
+++ neuralop/losses/score_helpers.py
@@ -20,7 +20,6 @@ def compute_deterministic_scores(
     scores_abs = {}
     scores_rel = {}
     for loss_name in losses:
-        #print(f'{loss_name}, {losses[loss_name]}')
         scores_abs[loss_name] = 0.0
         scores_rel[loss_name] = 0.0
     
@@ -43,7 +42,8 @@ def print_scores(
     scores_abs=None,
     scores_rel=None,
     reductions=None,
-    probScores=None,
+    probScoresIn=None,
+    probScoresOut=None,
 ):
     """
     Prints rel, abs and probabilistic scores.
@@ -54,7 +54,7 @@ def print_scores(
         if reductions is not None:
             print(f"\nDeterministic Scores (reductions: {reductions}):")
         else:
-            print(f"\nDeterministic Scores:")
+            print("\nDeterministic Scores:")
         if scores_abs is not None:
             det_row1 = list(scores_abs.keys())
         else:
@@ -75,109 +75,198 @@ def print_scores(
             det_table = [det_row1, det_row3]
         print(tabulate(det_table, headers='firstrow', tablefmt='fancy_grid'))
     
-    if probScores is not None:
+    if probScoresIn is not None or probScoresOut is not None:
         print("\nProbabilistic Scores:")
-        prob_row1 = list(probScores.keys())
-        prob_row2 = list(probScores.values())
-        prob_table = [prob_row1, prob_row2]
+        if probScoresIn is not None:
+            prob_row1 = list(probScoresIn.keys())
+        else:
+            prob_row1 = list(probScoresOut.keys())
+        prob_row1.insert(0, '-')
+        if probScoresIn is not None:
+            prob_row2 = list(probScoresIn.values())
+            prob_row2.insert(0, "InOfDist")
+        if probScoresOut is not None:
+            prob_row3 = list(probScoresOut.values())
+            prob_row3.insert(0, "OutOfDist")
+        if probScoresIn is not None:
+            if probScoresOut is not None:
+                prob_table = [prob_row1, prob_row2, prob_row3]
+            else:
+                prob_table = [prob_row1, prob_row2]
+        else:
+            prob_table = [prob_row1, prob_row3]
         print(tabulate(prob_table, headers='firstrow', tablefmt='fancy_grid'))
     
 def plot_scores(
     scores_det=None,
-    scores_prob=None,
+    scores_probIn=None,
+    scores_probOut=None,
     train_losses=None,
     batchSize=None,
     trainSetSize=None,
     save_folder=None,
-    initial_eval=True
+    initial_eval=True,
+    lastEpoch=0,
 ):
     """
-    scores_det and scores_prob dictionary, train_losses list. If train_losses is given then batch size and set size must be given.
+    Create plots of evolution of given scores over epochs.
+    Separate subplots for deterministic and probabilistic errors.
+    scores_det and scores_probIn/Out: dictionary, train_losses: list. If train_losses is given then batch size and set size must be given.
     """
     
     # Also put out the raw data
-    writeData(scores_det, scores_prob, train_losses, save_folder, initial_eval)
+    writeData(scores_det, scores_probIn, scores_probOut, train_losses, save_folder, initial_eval)
     
-    if scores_det[0] is None and scores_prob[0] is not None:
+    if scores_det[0] is None and (scores_probIn[0] is not None or scores_probOut is not None):
         if len(train_losses) == 0:
             fig, axs = plt.subplots(1)
             fig.tight_layout(pad=3.0)
             fig.set_size_inches(8,7)
-            probMat = dictToArray(scores_prob=scores_prob)
-            for scoreIdx in range(probMat.shape[0]):
-                x = np.arange(probMat.shape[1])
-                axs.plot(x, probMat[scoreIdx,:], label=list(scores_prob[0].keys())[scoreIdx])
+            if scores_probIn is not None:
+                probMatIn = dictToArray(scores_prob=scores_probIn)
+            if scores_probOut is not None:
+                probMatOut = dictToArray(scores_prob=scores_probOut)
+            if not initial_eval:
+                if scores_probIn is not None:
+                    pad = np.full(probMatIn.shape[0], nP.NaN)
+                    probMatIn = np.column_stack((pad, probMatIn))
+                if scores_probOut is not None:
+                    pad = np.full(probMatOut.shape[0], nP.NaN)
+                    probMatOut = np.column_stack((pad, probMatOut))
+            if scores_probIn is not None:
+                numProbLosses = probMatIn.shape[0]
+                numProbEpochs = probMatIn.shape[1]
+            else:
+                numProbLosses = probMatOut.shape[0]
+                numProbEpochs = probMatOut.shape[1]
+            for scoreIdx in range(numProbLosses):
+                x = np.arange(lastEpoch, lastEpoch+numProbEpochs)
+                if scores_probIn is not None:
+                    axs.plot(x, probMatIn[scoreIdx,:], label=list(scores_probIn[0].keys())[scoreIdx])
+                if scores_probOut is not None:
+                    axs.plot(x, probMatOut[scoreIdx,:], label=list(scores_probOut[0].keys())[scoreIdx], linestyle='dashed')
+            axs.set_yscale('log')
+            axs.grid(True)
             axs.set(xlabel='Epoch', ylabel='Error')
-            axs.set_xticks(range(probMat.shape[1]))
+            axs.set_xticks(range(lastEpoch, lastEpoch+numProbEpochs))
             axs.legend()
             axs.set_title('Probabilistic errors')
         else:
             fig, axs = plt.subplots(2)
             fig.tight_layout(pad=3.0)
             fig.set_size_inches(8,7)
-            probMat = dictToArray(scores_prob=scores_prob)
-            for scoreIdx in range(probMat.shape[0]):
-                x = np.arange(probMat.shape[1])
-                axs[1].plot(x, probMat[scoreIdx,:], label=list(scores_prob[0].keys())[scoreIdx])
+            if scores_probIn is not None:
+                probMatIn = dictToArray(scores_prob=scores_probIn)
+            if scores_probOut is not None:
+                probMatOut = dictToArray(scores_prob=scores_probOut)
+            if not initial_eval:
+                if scores_probIn is not None:
+                    pad = np.full(probMatIn.shape[0], nP.NaN)
+                    probMatIn = np.column_stack((pad, probMatIn))
+                if scores_probOut is not None:
+                    pad = np.full(probMatOut.shape[0], nP.NaN)
+                    probMatOut = np.column_stack((pad, probMatOut))
+            if scores_probIn is not None:
+                numProbLosses = probMatIn.shape[0]
+                numProbEpochs = probMatIn.shape[1]
+            else:
+                numProbLosses = probMatOut.shape[0]
+                numProbEpochs = probMatOut.shape[1]
+            for scoreIdx in range(numProbLosses):
+                x = np.arange(lastEpoch, lastEpoch+numProbEpochs)
+                if scores_probIn is not None:
+                    axs[1].plot(x, probMatIn[scoreIdx,:], label=list(scores_probIn[0].keys())[scoreIdx])
+                if scores_probOut is not None:
+                    axs[1].plot(x, probMatOut[scoreIdx,:], label=list(scores_probOut[0].keys())[scoreIdx], linestyle='dashed')
+            axs[1].set_yscale('log')
+            axs[1].grid(True)
             axs[1].set(xlabel='Epoch', ylabel='Error')
-            axs[1].set_xticks(range(probMat.shape[1]))
+            axs[1].set_xticks(range(lastEpoch, lastEpoch+numProbEpochs))
             axs[1].legend()
             axs[1].set_title('Probabilistic errors')
         
-    elif scores_det[0] is not None and scores_prob[0] is None:
+    elif scores_det[0] is not None and (scores_probIn[0] is None and scores_probOut is None):
         fig, axs = plt.subplots(1)
         fig.tight_layout(pad=3.0)
         fig.set_size_inches(8,7)
         detMat = dictToArray(scores_det=scores_det)
+        if not initial_eval:
+            pad = np.full(detMat.shape[0], nP.NaN)
+            detMat = np.column_stack((pad, detMat))
         for scoreIdx in range(detMat.shape[0]):
-            x = np.arange(detMat.shape[1])
+            x = np.arange(lastEpoch, lastEpoch+detMat.shape[1])
             axs.plot(x, detMat[scoreIdx,:], label=list(scores_det[0].keys())[scoreIdx])
+        axs.set_yscale('log')
+        axs.grid(True)
         axs.set(xlabel='Epoch', ylabel='Error')
-        axs.set_xticks(range(detMat.shape[1]))
+        axs.set_xticks(range(lastEpoch, lastEpoch+detMat.shape[1]))
         axs.legend()
         axs.set_title('Deterministic errors')
         
-    elif scores_det[0] is not None and scores_prob[0] is not None:
+    elif scores_det[0] is not None and (scores_probIn[0] is not None or scores_probOut is not None):
         fig, axs = plt.subplots(2)
         fig.tight_layout(pad=3.0)
         fig.set_size_inches(8,7)
-        detMat, probMat = dictToArray(scores_det=scores_det, scores_prob=scores_prob)
+        detMat = dictToArray(scores_det=scores_det)
+        if scores_probIn is not None:
+            probMatIn = dictToArray(scores_prob=scores_probIn)
+        if scores_probOut is not None:
+            probMatOut = dictToArray(scores_prob=scores_probOut)
+        if not initial_eval:
+            if scores_probIn is not None:
+                pad = np.full(probMatIn.shape[0], nP.NaN)
+                probMatIn = np.column_stack((pad, probMatIn))
+            if scores_probOut is not None:
+                pad = np.full(probMatOut.shape[0], nP.NaN)
+                probMatOut = np.column_stack((pad, probMatOut))
+        if not initial_eval:
+                pad = np.full(detMat.shape[0], nP.NaN)
+                detMat = np.column_stack((pad, detMat))
+        if scores_probIn is not None:
+            numProbLosses = probMatIn.shape[0]
+            numProbEpochs = probMatIn.shape[1]
+        else:
+            numProbLosses = probMatOut.shape[0]
+            numProbEpochs = probMatOut.shape[1]
         for scoreIdx in range(detMat.shape[0]):
-            x = np.arange(detMat.shape[1])
+            x = np.arange(lastEpoch, lastEpoch+detMat.shape[1])
             axs[0].plot(x, detMat[scoreIdx,:], label=list(scores_det[0].keys())[scoreIdx])
-        for scoreIdx in range(probMat.shape[0]):
-            x = np.arange(probMat.shape[1])
-            axs[1].plot(x, probMat[scoreIdx,:], label=list(scores_prob[0].keys())[scoreIdx])
+        for scoreIdx in range(numProbLosses):
+            x = np.arange(lastEpoch, lastEpoch+numProbEpochs)
+            if scores_probIn is not None:
+                axs[1].plot(x, probMatIn[scoreIdx,:], label=list(scores_probIn[0].keys())[scoreIdx])
+            if scores_probOut is not None:
+                axs[1].plot(x, probMatOut[scoreIdx,:], label=list(scores_probOut[0].keys())[scoreIdx], linestyle='dashed')
         for ax in axs.flat:
+            ax.set_yscale('log')
+            ax.grid(True)
             ax.set(xlabel='Epoch', ylabel='Error')
-            ax.set_xticks(range(detMat.shape[1]))
+            ax.set_xticks(range(lastEpoch, lastEpoch+detMat.shape[1]))
             ax.legend()
         axs[0].set_title('Deterministic errors')
         axs[1].set_title('Probabilistic errors')
     
     if len(train_losses) != 0:
-        if scores_det[0] is not None and scores_prob[0] is None:
-            if initial_eval:
-                pad = [np.NaN] * math.ceil(trainSetSize/batchSize)
-                train_losses = pad + train_losses
+        if scores_det[0] is not None and (scores_probIn[0] is None and scores_probOut is None):
             x_loss = np.linspace(0, len(scores_det)-1, len(train_losses))
             axs.plot(x_loss, train_losses, label='Training loss')
             axs.legend()
-        elif scores_det[0] is None and scores_prob[0] is None:
+        elif scores_det[0] is None and (scores_probIn[0] is None and scores_probOut is None):
             fig, axs = plt.subplots(1)
             fig.tight_layout(pad=3.0)
             fig.set_size_inches(8,7)
             x_loss = np.linspace(0, len(train_losses), len(train_losses))
             axs.plot(x_loss, train_losses, label='Training loss')
+            axs.set_yscale('log')
+            axs.grid(True)
             axs.set(xlabel='Batch', ylabel='Loss')
             axs.set_title('Trainig loss')
             axs.legend()
         else:
-            if initial_eval:
-                pad = [np.NaN] * math.ceil(trainSetSize/batchSize)
-                train_losses = pad + train_losses
             x_loss = np.linspace(0, len(scores_det)-1, len(train_losses))
             axs[0].plot(x_loss, train_losses, label='Training loss')
+            axs[0].set_yscale('log')
+            axs[0].grid(True)
             axs[0].legend()
     
     if save_folder is None:
@@ -191,6 +280,9 @@ def dictToArray(
     scores_prob=None
 ):
     """
+    Helper function to turn a dictionary of scores into an array.
+    Separate arrays for deterministic and probabilistic.
+    array.size = [num of epochs, num of different scores] (I think...)
     """
     if scores_det is not None and scores_prob is not None:
         detMat = np.zeros((len(scores_det[0].values()), len(scores_det)))
@@ -227,8 +319,9 @@ def dictToArray(
                 
         return detMat
 
-def writeData(scores_det, scores_prob, train_losses, save_folder, initial_eval):
+def writeData(scores_det, scores_probIn, scores_probOut, train_losses, save_folder, initial_eval):
     """
+    Prints scores.
     """
     if save_folder is None:
         file = open('train_errors_data.txt', 'w')
@@ -253,17 +346,52 @@ def writeData(scores_det, scores_prob, train_losses, save_folder, initial_eval):
             
         file.write('\n')
         
-    if scores_prob[0] is not None:
+    if scores_probIn[0] is not None and scores_probOut is not None:
         file.write('\nProbabilistic scores\n')
-        for loss in scores_prob[0]:
+        for loss in scores_probIn[0]:
             file.write(loss+' ')
         file.write('\n')
 
-        for dict in scores_prob:
+        file.write('InDist\n')
+        for dict in scores_probIn:
+            file.write('\n')
+            for loss in dict:
+                file.write(f'{dict[loss]} ')
+
+        file.write('\nInDist\n')
+        for dict in scores_probOut:
             file.write('\n')
             for loss in dict:
                 file.write(f'{dict[loss]} ')
         
+        file.write('\n')
+
+    elif scores_probIn[0] is not None and scores_probOut is None:
+        file.write('\nProbabilistic scores\n')
+        for loss in scores_probIn[0]:
+            file.write(loss+' ')
+        file.write('\n')
+
+        file.write('InDist\n')
+        for dict in scores_probIn:
+            file.write('\n')
+            for loss in dict:
+                file.write(f'{dict[loss]} ')
+
+        file.write('\n')
+
+    elif scores_probIn[0] is None and scores_probOut is not None:
+        file.write('\nProbabilistic scores\n')
+        for loss in scores_probOut[0]:
+            file.write(loss+' ')
+        file.write('\n')
+
+        file.write('InDist\n')
+        for dict in scores_probOut:
+            file.write('\n')
+            for loss in dict:
+                file.write(f'{dict[loss]} ')
+
         file.write('\n')
         
     if len(train_losses) != 0:
