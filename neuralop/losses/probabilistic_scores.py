a092a11ddf4c91fea58f1e4f81d5f8b96d29f921
fstuehlinger <fstuehlinger@student.ethz.ch>
2024-09-10 18:36:48 +0200

    Final version with comments

--- neuralop/losses/probabilistic_scores.py
+++ neuralop/losses/probabilistic_scores.py
@@ -9,6 +9,9 @@ import xskillscore as xs
 import scoringrules as sr
 import numpy as np
 import math
+import sys
+import psutil
+import gc
 
 class gaussian_crps(object):
     """
@@ -91,12 +94,15 @@ class hacky_crps(object):
         NO_crps = 0.0
         Num_crps = 0.0
         
+        # Compute means
         x_mu = torch.mean(ensemble_x, self.member_dim).detach()
         y_mu = torch.mean(ensemble_y, self.member_dim).detach()
         
+        # Compute stds
         x_sigma = torch.std(ensemble_x, dim=self.member_dim).detach()
         y_sigma = torch.std(ensemble_y, dim=self.member_dim).detach()
         
+        # Compute CRPS for predicted and ground truth ensemble. Indexing dependent on member dim
         if self.member_dim == 0:
             for i in range(100):
                 NO_crps += (1./100.)*np.mean(sr.crps_normal(x_mu, x_sigma, ensemble_y[i,:,:,:].detach()), self.reduce_dims)
@@ -114,6 +120,7 @@ class hacky_crps(object):
                 NO_crps += (1./100.)*np.mean(sr.crps_normal(x_mu, x_sigma, ensemble_y[:,:,:,i].detach()), self.reduce_dims)
                 Num_crps += (1./100.)*np.mean(sr.crps_normal(y_mu, y_sigma, ensemble_y[:,:,:,i].detach()), self.reduce_dims)
         
+        # Take difference of ground truth and prediction crps
         return abs(NO_crps - Num_crps)
 
 class lognormal_crps(object):
@@ -140,13 +147,13 @@ class lognormal_crps(object):
         x_data = ensemble_x.detach().numpy().copy()
         y_data = ensemble_y.detach().numpy().copy()
         
-        # Mean and standard dev. for normalization
+        # Mean and standard dev. for standardization
         x_mu_old = np.mean(x_data, axis=self.member_dim)
         x_sigma_old = np.std(x_data, axis=self.member_dim)
         y_mu_old = np.mean(y_data, axis=self.member_dim)
         y_sigma_old = np.std(y_data, axis=self.member_dim)
         
-        # Normalize
+        # Standardize
         if self.member_dim == 0:
             for i in range(100):
                 x_data[i,:,:,:] = (x_data[i,:,:,:] - x_mu_old) / x_sigma_old
@@ -192,6 +199,7 @@ class lognormal_crps(object):
         x_sigma = np.std(x_data, axis=self.member_dim)
         y_sigma = np.std(y_data, axis=self.member_dim)
         
+        # Compute CRPS for predicted and ground truth ensemble. Indexing dependent on member dim
         if self.member_dim == 0:
             for i in range(100):
                 NO_crps += (1./100.)*np.mean(sr.crps_lognormal(x_mu, x_sigma, y_data[i,:,:,:]), self.reduce_dims)
@@ -209,6 +217,7 @@ class lognormal_crps(object):
                 NO_crps += (1./100.)*np.mean(sr.crps_lognormal(x_mu, x_sigma, y_data[:,:,:,i]), self.reduce_dims)
                 Num_crps += (1./100.)*np.mean(sr.crps_lognormal(y_mu, y_sigma, y_data[:,:,:,i]), self.reduce_dims)
         
+        # Take difference of ground truth and prediction crps
         return abs(NO_crps - Num_crps)
 
 
@@ -233,6 +242,7 @@ class ensemble_crps(object):
         NO_crps = 0.0
         Num_crps = 0.0
         
+        # Compute CRPS for predicted and ground truth ensemble. Indexing dependent on member dim
         if self.member_dim == 0:
             for i in range(100):
                 NO_crps += (1./100.)*np.mean(sr.crps_ensemble(ensemble_x.detach(), ensemble_y[i,:,:,:].detach(), axis=0), self.reduce_dims)
@@ -250,6 +260,7 @@ class ensemble_crps(object):
                 NO_crps += (1./100.)*np.mean(sr.crps_ensemble(ensemble_x.detach(), ensemble_y[:,:,:,i].detach(), axis=3), self.reduce_dims)
                 Num_crps += (1./100.)*np.mean(sr.crps_ensemble(ensemble_y.detach(), ensemble_y[:,:,:,i].detach(), axis=3), self.reduce_dims)
         
+        # Take difference of ground truth and prediction crps
         return abs(NO_crps - Num_crps)
     
 
@@ -272,13 +283,15 @@ class mmd(object):
             
     def eval(self, ensemble_x, ensemble_y):
         """
-        X and Y both need to be ensembles.
+        X and Y both need to be ensembles. Computation according to formula here: https://www.youtube.com/watch?v=zFffYuDGslg (7:50)
         """
         xx_mean = 0.0
         yy_mean = 0.0
         xy_mean = 0.0
         ensemble_size = ensemble_x.shape[self.member_dim]
         count = 0
+
+        # Compute different means neccessary for computing mmd. Indexing dependent on member dim
         if self.member_dim == 0:
             for i in range(ensemble_size):
                 for j in range(i, ensemble_size):
@@ -310,13 +323,14 @@ class mmd(object):
         xx_mean /= count
         yy_mean /= count
         xy_mean /= count
+
         return torch.as_tensor(math.sqrt(xx_mean + yy_mean - 2*xy_mean))
         
     
 # Kernels for mmd
 class rbf(object):
     """
-    Gaussian kernel for mmd.
+    Gaussian kernel for mmd. Formula: exp(-(||x-y||²) / sigma²)
     """
     def __init__(self, sigma):
         super().__init__()
@@ -343,49 +357,22 @@ def compute_probabilistic_scores(
     """  
     scores = {}
     for loss_name in losses:
-        #print(f'{loss_name}, {losses[loss_name]}')
         scores[loss_name] = 0.0
     
-    # Compute score for all losses, averaged over all ensembles # Ensemble wise approach
-    #for index in range(10):
-    #    ensemble = test_db.get_ensemble(index)
-        
-        # Get truth and inputs
-    #    inputs, truth = test_db.ensembleInParts(ensemble)
-        
-        # Predict
-    #    predictions = evaluate_ensemble(model, inputs)
-        
-        #data = data_processor.preprocess(data, batched=False)
-        #y = data['y'].squeeze()
-        #out = model(data['x'].unsqueeze(0))
-        # Make sure data is on cpu
-        #y = y.to(device='cpu')
-        #out = out.to(device='cpu')
-        #if deterministic is True:
-        #    out.unsqueeze(0)
-        
-    #    for loss_name in losses:
-            # Average over all ensembles
-    #        scores[loss_name] += (1./n)*(losses[loss_name].eval(predictions, truth)).item()
-    
-    # "Normla approach"
+    # Loop over all ensembles
     for ens_idx in range(10):
         
         ensemble_x = torch.empty((100,2,128,128))
         ensemble_y = torch.empty((100,2,128,128))
         
+        # Get predictions and ground truths for all 100 members
         for sample_idx in range(100):
             data = test_db[ens_idx*100 + sample_idx]
             data = data_processor.preprocess(data, batched=False)
-            #y = data['y'].squeeze()
-            #out = model(data['x'].unsqueeze(0))
-            # Make sure data is on cpu
-            #y = y.to(device='cpu')
-            #out = out.to(device='cpu')
             ensemble_x[sample_idx,:,:,:] = model(data['x'].unsqueeze(0)).to(device='cpu')
             ensemble_y[sample_idx,:,:,:] = data['y'].squeeze().to(device='cpu')
             
+        # Compute all losses for this ensemble
         for loss_name in losses:
             scores[loss_name] += (1./10)*(losses[loss_name].eval(ensemble_x, ensemble_y)).item()
             
@@ -396,6 +383,7 @@ def evaluate_ensemble(
     inputs,
 ):
     """
+    UNFINISHED
     Takes ensemble of input values and returns predicitons for all micro members.
     Arguments:
         model: the model to evaluate the data with
@@ -414,6 +402,8 @@ def hacky_crps_comp(
     ensemble_y
 ):
     """
+    NEVER USED
+    Function for computing gaussian crps for an ensemble.
     """
     NO_crps = 0.0
     Num_crps = 0.0
@@ -429,3 +419,176 @@ def hacky_crps_comp(
         Num_crps += (1./100.)*np.mean(sr.crps_normal(y_mu, y_sigma, ensemble_y[i,:,:,:].detach()))
         
     return NO_crps, Num_crps
+
+
+def print_memory_usage():
+    process = psutil.Process()
+    mem_info = process.memory_info()
+    print(f"Memory usage: {mem_info.rss / (1024 ** 2)} MB")
+
+
+def baseline_crps(model, dataset, data_processor, set_size):
+    """
+    Function for computing the baseline crps. dataset is expected to be the regular shear data (baseline dataset).
+    """
+
+    model.eval()
+
+    sqrtSize = math.sqrt(float(set_size))
+    if not sqrtSize.is_integer():
+        raise Exception("No integer square root.")
+    sqrtSize = int(sqrtSize)
+    ensemble_x = torch.empty((sqrtSize,2,128,128))
+    ensemble_y = torch.empty((sqrtSize,2,128,128))
+
+    NO_crps = 0.0
+    Num_crps = 0.0
+
+    # Compute as average of sqrtSize number of "artificial" ensembles instead of one huge ensemble
+    # (in order to use less memory)
+    for i in range(sqrtSize):
+
+        for sample_idx in range(sqrtSize):
+            data = dataset[i*sqrtSize + sample_idx]
+            data = data_processor.preprocess(data, batched=False)
+            out = model(data['x'].unsqueeze(0)).to(device='cpu')
+            out, data = data_processor.postprocess(out, data)
+            ensemble_x[sample_idx,:,:,:] = out
+            ensemble_y[sample_idx,:,:,:] = data['y'].squeeze().to(device='cpu')
+        
+        for i in range(sqrtSize):
+            NO_crps += (1./float(set_size))*np.mean(sr.crps_ensemble(ensemble_x.detach(), ensemble_y[i,:,:,:].detach(), axis=0), None)
+            Num_crps += (1./float(set_size))*np.mean(sr.crps_ensemble(ensemble_y.detach(), ensemble_y[i,:,:,:].detach(), axis=0), None)
+    
+        # Clear intermediate results and free up memory
+        ensemble_x.detach_()
+        ensemble_y.detach_()
+        gc.collect()
+        print_memory_usage()
+
+    return abs(NO_crps - Num_crps)
+
+
+def baseline_mmd(model, dataset, data_processor, set_size):
+    """
+    Function for computing the baseline mmd. dataset is expected to be the regular shear data (baseline dataset).
+    """
+    #sigma = 0.0313839316368103
+    sigma = -0.027283422648906708
+    print(f'Gauss kernel sigma for mmd: {sigma}')
+    gauss_kernel = rbf(sigma)
+    maxMeanDiscr = mmd(gauss_kernel, member_dim=0, reduce_dims=None)
+
+    model.eval()
+
+    sqrtSize = math.sqrt(float(set_size))
+    if not sqrtSize.is_integer():
+        raise Exception("No integer square root.")
+    sqrtSize = int(sqrtSize)
+
+    ensemble_x = torch.empty((sqrtSize,2,128,128))
+    print(ensemble_x.shape)
+    ensemble_y = torch.empty((sqrtSize,2,128,128))
+
+    MMD = 0
+
+    for i in range(sqrtSize):
+
+        for sample_idx in range(sqrtSize):
+            data = dataset[i*sqrtSize + sample_idx]
+            data = data_processor.preprocess(data, batched=False)
+            ensemble_x[sample_idx,:,:,:] = model(data['x'].unsqueeze(0)).to(device='cpu')
+            ensemble_y[sample_idx,:,:,:] = data['y'].squeeze().to(device='cpu')
+    
+        MMD += (1./float(sqrtSize))*(maxMeanDiscr.eval(ensemble_x, ensemble_y)).item()
+
+        # Clear intermediate results and free up memory
+        ensemble_x.detach_()
+        ensemble_y.detach_()
+        gc.collect()
+        print_memory_usage()
+
+    
+    return MMD
+
+
+def singleEnsemble_baseline_crps(model, dataset, data_processor, set_size):
+    """
+    Same as baseline_crps but with one huge ensemble.
+    Needs a lot of memory for big set_size.
+    """
+
+    model.eval()
+
+    ensemble_x = torch.empty((set_size,2,128,128))
+    print(ensemble_x.shape)
+    ensemble_y = torch.empty((set_size,2,128,128))
+
+    NO_crps = 0.0
+    Num_crps = 0.0
+
+    for sample_idx in range(set_size):
+        data = dataset[sample_idx]
+        data = data_processor.preprocess(data, batched=False)
+        out = model(data['x'].unsqueeze(0)).to(device='cpu')
+        out, data = data_processor.postprocess(out, data)
+        ensemble_x[sample_idx,:,:,:] = out
+        ensemble_y[sample_idx,:,:,:] = data['y'].squeeze().to(device='cpu')
+        if sample_idx % 100 == 0:
+            print(sample_idx)
+    
+    for i in range(set_size):
+        NO_crps += (1./float(set_size))*np.mean(sr.crps_ensemble(ensemble_x.detach(), ensemble_y[i,:,:,:].detach(), axis=0), None)
+        Num_crps += (1./float(set_size))*np.mean(sr.crps_ensemble(ensemble_y.detach(), ensemble_y[i,:,:,:].detach(), axis=0), None)
+        if i % 100 == 0:
+            print(i)
+    
+    # Clear intermediate results and free up memory
+    ensemble_x.detach_()
+    ensemble_y.detach_()
+    gc.collect()
+    print_memory_usage()
+
+    return abs(NO_crps - Num_crps)
+
+
+def singleEnsemble_baseline_mmd(model, dataset, data_processor, set_size):
+    """
+    Same as baseline_mmd but with one huge ensemble.
+    Needs a lot of memory for big set_size.
+    """
+    #sigma = 0.0313839316368103
+    sigma = -0.027283422648906708
+    print(f'Gauss kernel sigma for mmd: {sigma}')
+    gauss_kernel = rbf(sigma)
+    maxMeanDiscr = mmd(gauss_kernel, member_dim=0, reduce_dims=None)
+
+    model.eval()
+
+    ensemble_x = torch.empty((set_size,2,128,128))
+    print(ensemble_x.shape)
+    ensemble_y = torch.empty((set_size,2,128,128))
+
+    MMD = 0
+
+    for sample_idx in range(set_size):
+        data = dataset[sample_idx]
+        data = data_processor.preprocess(data, batched=False)
+        out = model(data['x'].unsqueeze(0)).to(device='cpu')
+        out, data = data_processor.postprocess(out, data)
+        ensemble_x[sample_idx,:,:,:] = out
+        ensemble_y[sample_idx,:,:,:] = data['y'].squeeze().to(device='cpu')
+        if sample_idx % 100 == 0:
+            print(sample_idx)
+
+    MMD = (1./float(set_size))*(maxMeanDiscr.eval(ensemble_x, ensemble_y)).item()
+
+
+    # Clear intermediate results and free up memory
+    ensemble_x.detach_()
+    ensemble_y.detach_()
+    gc.collect()
+    print_memory_usage()
+
+    
+    return MMD
