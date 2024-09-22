a04fc31b6ef9b3d9fb198f7c33fa80d83c0e30be
fstuehlinger <fstuehlinger@student.ethz.ch>
2024-09-22 18:54:07 +0200

    Final version

--- neuralop/losses/__init__.py
+++ neuralop/losses/__init__.py
@@ -1,5 +1,5 @@
 from .data_losses import LpLoss, H1Loss, MedianAbsoluteLoss
 from .equation_losses import BurgersEqnLoss, ICLoss
 from .meta_losses import WeightedSumLoss
-from .probabilistic_scores import gaussian_crps, compute_probabilistic_scores, evaluate_ensemble, hacky_crps, lognormal_crps, ensemble_crps, mmd, rbf
+from .probabilistic_scores import gaussian_crps, compute_probabilistic_scores, evaluate_ensemble, hacky_crps, lognormal_crps, ensemble_crps, mmd, rbf, baseline_crps, baseline_mmd, singleEnsemble_baseline_crps
 from .score_helpers import compute_deterministic_scores, print_scores, plot_scores
