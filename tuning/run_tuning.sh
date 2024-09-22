907ed46f8589b73916d7abe99e1de8e774db035f
fstuehlinger <fstuehlinger@student.ethz.ch>
2024-09-22 18:57:07 +0200

    Final version

--- tuning/run_tuning.sh
+++ tuning/run_tuning.sh
@@ -4,10 +4,11 @@
 #SBATCH --gpus=2
 #SBATCH --mem-per-cpu=32G
 #SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
 #SBATCH --job-name=Tuning
-#SBATCH --output=tuning_n_train=400_n_epochs=5_1cpu_2gpu.out
-#SBATCH --error=tuning_n_train=400_n_epochs=5_1cpu_2gpu.err
-#SBATCH --time=4:0:0
+#SBATCH --output=tuning_n_train=10000_n_epochs=5_3cpu_2gpu.out
+#SBATCH --error=tuning_n_train=10000_n_epochs=5_3cpu_2gpu.err
+#SBATCH --time=6-0:00:00
 
 module load gcc/8.2.0 openblas/0.3.20 cuda/11.8.0 cudnn/8.8.1.3 nccl/2.11.4-1 python_gpu/3.11.2
 source ~/venv-NO-ens/bin/activate
